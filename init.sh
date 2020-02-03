14a4f39720954e4d93727dbe3c1377a14344fd40
hanifr <irhaniframli@gmail.com>
2020-02-03 15:33:51 +0800

    Create init.sh
    deployment script.

---
+++ init.sh
@@ -0,0 +1,8 @@
+echo -e "${_CYAN}LATEX installation :: initiated"${_RESET}
+
+sudo apt install texlive-latex-extra
+sleep 5
+sudo apt install texstudio
+
+echo -e "${_CYAN}LATEX installation :: completed"${_RESET}
+sleep 5
